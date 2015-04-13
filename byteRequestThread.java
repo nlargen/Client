69fe008bd63e35ea2bd1c5443e1ec9a1a07edc22
Nicholas Largen <nicholaslargen@Nicholass-MacBook-Pro-2.local>
2015-04-13 11:38:29 -0500

    working on finished server side

--- byteRequestThread.java
+++ byteRequestThread.java
@@ -15,8 +15,9 @@ public class byteRequestThread extends clientHelperThread
 		{
 			
 			//*****Write Code HERE****
-			
-			if(theFileArray[i] != 1)
+			// not sure how to link it with the current byte we need
+			int i = 0; 
+			if(theFileArray[] != null)
 			{
 				//sends specific byte at index i out to the server 
 				FileOutputStream fos = new FileOutputStream("" + theFileArray[i]); 
